964204a50d3322942fcc169547417019ce85c365
kaylamuraoka <kmurs98@gmail.com>
2021-03-04 22:52:30 -1000

    adding a data directory in source folder with a file containing the data for the navigation links

---
+++ client/src/data/navigationRoutes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "About",
+    path: "/about",
+  },
+  {
+    label: "Resume",
+    path: "/resume",
+  },
+  {
+    label: "Projects",
+    path: "/projects",
+  },
+  {
+    label: "Contact",
+    path: "/contact",
+  },
+];
+
+export default routes;
