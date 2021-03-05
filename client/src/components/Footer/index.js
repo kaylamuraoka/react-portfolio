c1735673fe2837abb79237ff72c89fafee2b7ddd
kaylamuraoka <kmurs98@gmail.com>
2021-03-04 22:50:45 -1000

    adding footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer>
+      <span>Designed By Kayla Muraoka</span>
+    </footer>
+  );
+}
+
+export default Footer;
