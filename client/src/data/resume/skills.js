c1be71c1e0333ce29fde80fc4758ac9f16a5cba5
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 13:56:41 -1000

    adding skills data

---
+++ client/src/data/resume/skills.js
@@ -0,0 +1,102 @@
+const skills = [
+  {
+    title: "Javascript/jQuery",
+    competency: 3,
+    category: ["Web Development", "Languages", "Javascript"],
+  },
+  {
+    title: "Node.JS",
+    competency: 3,
+    category: ["Web Development", "Javascript"],
+  },
+  {
+    title: "React",
+    competency: 3,
+    category: ["Web Development", "Javascript"],
+  },
+  {
+    title: "Bash",
+    competency: 2,
+    category: ["Tools", "Languages"],
+  },
+  {
+    title: "Heroku",
+    competency: 2,
+    category: ["Web Development", "Tools"],
+  },
+  {
+    title: "MongoDB",
+    competency: 2,
+    category: ["Web Development", "Databases"],
+  },
+  {
+    title: "MySQL",
+    competency: 3,
+    category: ["Web Development", "Databases"],
+  },
+  {
+    title: "Express.JS",
+    competency: 3,
+    category: ["Web Development", "Javascript"],
+  },
+  {
+    title: "Numpy",
+    competency: 2,
+    category: ["Data Science", "Data Engineering", "Python"],
+  },
+  {
+    title: "HTML + CSS",
+    competency: 4,
+    category: ["Web Development", "Languages"],
+  },
+  {
+    title: "Python",
+    competency: 2,
+    category: ["Languages", "Python"],
+  },
+  {
+    title: "Pandas",
+    competency: 2,
+    category: ["Data Engineering", "Data Science", "Python"],
+  },
+  {
+    title: "Matplotlib",
+    competency: 2,
+    category: ["Data Engineering", "Data Science", "Python"],
+  },
+  {
+    title: "Bootstrap",
+    competency: 4,
+    category: ["Web Development"],
+  },
+  {
+    title: "Materialize",
+    competency: 3,
+    category: ["Web Development"],
+  },
+];
+
+// const colors = [
+//   "#6968b3",
+//   "#37b1f5",
+//   "#40494e",
+//   "#515dd4",
+//   "#e47272",
+//   "#cc7b94",
+//   "#3896e2",
+//   "#c3423f",
+//   "#d75858",
+//   "#747fff",
+//   "#64cb7b",
+// ];
+
+// const categories = [
+//   ...new Set(skills.reduce((acc, { category }) => acc.concat(category), [])),
+// ]
+//   .sort()
+//   .map((category, index) => ({
+//     name: category,
+//     color: colors[index],
+//   }));
+
+export default skills;
