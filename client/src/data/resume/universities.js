fcda1c0e1e704bda62dc823079c6c6fa3c84bf41
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 13:56:07 -1000

    adding universities data

---
+++ client/src/data/resume/universities.js
@@ -0,0 +1,35 @@
+const universities = [
+  {
+    school: "University of California, Berkeley",
+    location: "Berkeley, CA",
+    degrees: ["Certificate, Full Stack Web Development Bootcamp"],
+    link: "https://bootcamp.berkeley.edu/coding/",
+    dateRange: "Sep. 2020 - Mar. 2021",
+  },
+  {
+    school: "Santa Clara University",
+    location: "Santa Clara, CA",
+    degrees: [
+      "BSc, Management Information Systems",
+      "BA, Individual Studies - Emphasis in Japanese Business Studies",
+    ],
+    link: "https://www.scu.edu/",
+    dateRange: "Sep. 2017 - Aug. 2020",
+  },
+  {
+    school: "Nanzan University",
+    location: "Nagoya, Japan",
+    degrees: ["Study Abroad Program, Santa Clara University"],
+    link: "https://www.nanzan-u.ac.jp/English/academics/cjs/",
+    dateRange: "Sep. 2019 - Dec. 2019",
+  },
+  {
+    school: "Stevens Institute of Technology",
+    location: "Hoboken, NJ",
+    degrees: ["Engineering Major, transferred to Santa Clara University"],
+    link: "https://www.stevens.edu/",
+    dateRange: "Aug. 2016 - May. 2017",
+  },
+];
+
+export default universities;
