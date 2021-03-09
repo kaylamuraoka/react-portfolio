4d6543a7b0649904912129a08f7b30988ca9f1f8
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 13:53:14 -1000

    Adding data that has contact links

---
+++ client/src/data/contactLinks.js
@@ -0,0 +1,36 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons/faFacebookF";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons/faEnvelope";
+
+const links = [
+  {
+    link: "https://www.linkedin.com/in/kaylamuraoka",
+    label: "LinkedIn",
+    icon: faLinkedinIn,
+  },
+  {
+    link: "https://github.com/kaylamuraoka",
+    label: "Github",
+    icon: faGithub,
+  },
+  {
+    link: "https://www.facebook.com/people/Kayla-Lei-Muraoka/100005309084712",
+    label: "Facebook",
+    icon: faFacebookF,
+  },
+  {
+    link: "https://www.instagram.com/kaylamuraoka/",
+    label: "Instagram",
+    icon: faInstagram,
+  },
+
+  {
+    link: "mailto:kmurs98@gmail.com",
+    label: "Email",
+    icon: faEnvelope,
+  },
+];
+
+export default links;
