2a156f3077b3203e398d118b3b147d4f7bc96f50
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 13:45:05 -1000

    updating styling of footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "./style.css";
+import SocialIcons from "../SocialIcons";
+import NavbarLinks from "../NavbarLinks";
+import Row from "../Row";
+import Col from "../Col";
 
 function Footer() {
   return (
     <footer>
-      <span>Designed By Kayla Muraoka</span>
+      <Row>
+        <Col size={"12"}>
+          <ul className="footer-nav">
+            <NavbarLinks />
+          </ul>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={"12"}>
+          <ul className="footer-icons">
+            <SocialIcons />
+          </ul>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={"12"}>
+          <button className="footer-btn" onClick={() => window.scrollTo(0, 0)}>
+            Back to top
+          </button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col size={"12"}>
+          <h5 className="copyright-text">© Copyright 2020 Kayla Muraoka</h5>
+        </Col>
+      </Row>
     </footer>
   );
 }
