f2361113ba4b4ed734031d7dbfdf6856c8c058eb
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 13:59:15 -1000

    adding job data

---
+++ client/src/data/resume/jobs.js
@@ -0,0 +1,73 @@
+const jobs = [
+  {
+    company: "Fukunaga & Associates Inc",
+    location: "Honolulu, HI",
+    position: "Database Administrator",
+    link: "https://fukunagaengineers.com/",
+    dateRange: "Oct. 2020 - Present",
+    points: [
+      "Coordinating the migration of company data from existing spreadsheets to a new MySQL RDBMS using database queries.",
+      "Working closely with project managers to deliver a SQL database architecture design that meets business requirements.",
+      "Maintaining robust database infrastructure by updating records, performing validation, joining tables, and managing users.",
+    ],
+  },
+  {
+    company: "Santa Clara University Leavey School of Business",
+    location: "Santa Clara, CA",
+    position: "Student Ambassador",
+    link:
+      "https://www.scu.edu/business/undergraduates/community/leavey-ambassadors-program/",
+    dateRange: "Mar. 2019 - Aug. 2020",
+    points: [
+      "Directed 2,000+ prospective students at Open House, Preview Days, Orientation and Family Weekend, making students feel more comfortable and confident while visiting.",
+      "Addressed questions and concerns of parents and potential students by participating in informational panels.",
+    ],
+  },
+  {
+    company: "Ciocca Center for Innovation & Entrepreneurship",
+    location: "Santa Clara, CA",
+    position: "Research Intern",
+    link: "https://www.scu.edu/cioccacenter/",
+    dateRange: "Jun. 2019 - Aug. 2019",
+    points: [
+      "Proposed the concept for the school’s first Entrepalooza event to promote the entrepreneurship minor, now planned for 2021.",
+      'Worked in a team of 10 to facilitate programs for the university’s first "Bronco Venture Accelerator."',
+    ],
+  },
+  {
+    company: "Quay Australia",
+    location: "Santa Clara, CA",
+    position: "Sales Associate",
+    link: "https://www.quayaustralia.com/",
+    dateRange: "Apr. 2018 - Aug. 2019",
+    points: [
+      "Earned company recognition for the highest units per transaction (UPT) among all regional sales associates (Oct. 2018).",
+      "Offered superb customer service; facilitated deals; surpassed sales goals in one of Quay’s top grossing retail stores.",
+    ],
+  },
+  {
+    company: "Optia Partners",
+    location: "Tokyo, Japan",
+    position: "Sales Associate",
+    link: "https://www.quayaustralia.com/",
+    dateRange: "Jun. 2018 - Aug. 2018",
+    points: [
+      "Sourced and recommended candidates, utilized Bullhorn application tracking system, and arranged screening interviews.",
+      'Received the "Optia Internship Award" for completing the most sales pitch and phone appointment calls.',
+      "Sourced a qualified candidate for successful employment at a Japanese firm resulting in a $27,000 profit for the company.",
+    ],
+  },
+  {
+    company: "YMCA of Honolulu",
+    location: "Honolulu, HI",
+    position: "Group Leader",
+    link: "https://www.ymcahonolulu.org/",
+    dateRange: "Jun. 2017 - Sep. 2017",
+    points: [
+      "Organized three of the YMCA’s youth enrichment programs, and managed 40+ diverse students, aged 7 to 11.",
+      "Facilitated peer-to-peer connections as part of the overall YMCA program experience.",
+      "Planned and orchestrated age-appropriate activities, communicated daily reports to parents/guardians, and filled out accident reports, incident reports and attendance records when necessary.",
+    ],
+  },
+];
+export default jobs;
