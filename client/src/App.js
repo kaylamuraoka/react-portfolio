03c52565fe55e49a2b7c961470b2db850fdeaf3c
kaylamuraoka <kmurs98@gmail.com>
2021-03-09 14:04:40 -1000

    updating App.js to use compnents

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import { ViewportProvider } from "./utils/viewportContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1>This is the title</h1>
-        <h2>header 2</h2>
-        <button>Button Text</button>
-      </header>
-    </div>
+    <ViewportProvider>
+      <Router>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/react-portfolio/" component={Home} />
+          <Route exact path="/react-portfolio/about" component={About} />
+          <Route exact path="/react-portfolio/resume" component={Resume} />
+          <Route exact path="/react-portfolio/projects" component={Projects} />
+          <Route exact path="/react-portfolio/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </Router>
+    </ViewportProvider>
   );
 }
 
